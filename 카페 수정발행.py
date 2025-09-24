9fed2fe94fa2aeca159c9b7a091387773aa811ec
seo <shr941207@gmail.com>
2025-09-24 16:23:01 +0900

    v0.1.9 - 최종 완벽 자동 업데이트

--- 카페 수정발행.py
+++ 카페 수정발행.py
@@ -7,13 +7,13 @@
 - 라이선스 인증 시스템
 - 자동 업데이트 기능
 
-Version: 0.1.8
+Version: 0.1.9
 Author: License Manager
 Last Updated: 2025-09-25
 """
 
 # 🔢 버전 정보
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __build_date__ = "2025-09-25"
 __author__ = "License Manager"
 
